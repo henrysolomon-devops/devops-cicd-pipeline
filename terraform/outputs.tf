c1b16704d5bd478a97a814a21d6975bf92cd1fd0
Henry Solomon <henrysolomon982@gmail.com>
2026-08-08 16:05:54 -0700

    Remove terraform/ - fully replaced by cloudformation/ in this version

--- terraform/outputs.tf
+++
@@ -1,6 +0,0 @@
-# Now pointing at the Elastic IP instead of the instance's own public IP,
-# since that's the address that actually stays put across rebuilds.
-output "server_public_ip" {
-  description = "Static public IP address of the EC2 instance (Elastic IP)"
-  value       = aws_eip.server.public_ip
-}
